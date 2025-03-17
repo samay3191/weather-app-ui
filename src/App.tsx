9ffb20508451062b8d6839e6376e2606eb46bc78
Samay Bhavsar <samay3191@gmail.com>
2025-03-18 10:50:38 +1100

    Moved map component to features

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { HStack } from "@chakra-ui/react";
 import Filter from "./features/filter/Filter";
 import { Toaster } from "./components/ui/toaster";
-import MapContainer from "./features/mapContainer/MapContainer";
+import GoogleMapViewer from "./features/mapContainer/GoogleMapViewer";
 
 function App() {
   return (
     <>
       <HStack gap={"0"}>
         <Filter />
-        <MapContainer />
+        <GoogleMapViewer />
       </HStack>
       <Toaster />
     </>
