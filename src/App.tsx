bda0cac455aeb21d6a55ce7befd8c719ff86ed92
Samay Bhavsar <samay3191@gmail.com>
2025-03-17 19:27:45 +1100

    Created initial Ui structure using react + Vite + TS + chakra UI

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Box, Heading } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <Box p={4}>
+      <Heading>Sample Weather App</Heading>
+    </Box>
+  )
+}
+
+export default App
