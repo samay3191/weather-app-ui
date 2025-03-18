00f590ae87065ccc927fc77df56fc838ae813901
Samay Bhavsar <samay3191@gmail.com>
2025-03-18 12:20:44 +1100

    Added created by in the footer area

--- src/features/filter/Filter.tsx
+++ src/features/filter/Filter.tsx
@@ -1,9 +1,10 @@
 import { useStore } from "@/store/store";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Link, Text, VStack } from "@chakra-ui/react";
 import { useEffect } from "react";
 import StateSelector from "./StateSelector";
 import ClearFilterButton from "./ClearFilterButton";
 import StationSelector from "./StationSelector";
+import { LuFilter } from "react-icons/lu";
 
 const Filter = () => {
   const stations = useStore((state) => state.stations);
@@ -16,12 +17,40 @@ const Filter = () => {
   }, [stations, fetchStations]);
 
   return (
-    <Box p={"4"} shadow={"lg"} height={"100vh"} zIndex={10}>
-      <Heading pb={"4"}>Filters</Heading>
-      <StateSelector />
-      <StationSelector />
-      <ClearFilterButton />
-    </Box>
+    <VStack
+      p={"4"}
+      shadow={"lg"}
+      height={"100vh"}
+      zIndex={10}
+      justify={"space-between"}
+    >
+      <Box alignItems={"baseline"}>
+        <Heading pb={"4"} display={"flex"} alignItems={"center"}>
+          <LuFilter />
+          <Text pl={"2"}>Station Filters</Text>
+        </Heading>
+        <StateSelector />
+        <StationSelector />
+        <ClearFilterButton />
+      </Box>
+      <Text
+        color={"gray.400"}
+        fontSize={"xs"}
+        textAlign={"left"}
+        alignSelf={"baseline"}
+      >
+        Created by{" "}
+        <Link
+          color={"gray.400"}
+          _hover={{ color: "blue.400" }}
+          fontWeight={"semibold"}
+          href="https://www.linkedin.com/in/samay-bhavsar-02642254/"
+          target="_blank"
+        >
+          Samay.Bhavsar
+        </Link>
+      </Text>
+    </VStack>
   );
 };
 
