db47db44201233d6b00c318aeeae79c266c4b975
Samay Bhavsar <samay3191@gmail.com>
2025-03-19 20:00:04 +1100

    fetching token on successful login and setting into store

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,23 @@ import GoogleMapViewer from "./features/mapContainer/GoogleMapViewer";
 import Header from "./components/Header";
 import { useAuth0 } from "@auth0/auth0-react";
 import Login from "./features/auth/Login";
+import { useCallback, useEffect } from "react";
+import { useStore } from "./store/store";
 
 function App() {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const setToken = useStore((state) => state.setToken);
+
+  const setUpToken = useCallback(async () => {
+    const token: string = await getAccessTokenSilently();
+    setToken(token);
+  }, [getAccessTokenSilently, setToken]);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      setUpToken();
+    }
+  }, [isAuthenticated, setUpToken]);
 
   if (!isAuthenticated) {
     return <Login />;
