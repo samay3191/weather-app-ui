6618a20d0f1a30f3bba72c0593e243c5b7de36ed
Samay Bhavsar <samay3191@gmail.com>
2025-03-19 20:19:40 +1100

    Added user prop in auth reducer

--- src/features/auth/reducer/authReducer.ts
+++ src/features/auth/reducer/authReducer.ts
@@ -1,9 +1,16 @@
+import { User } from "@auth0/auth0-react";
+import { StateCreator } from "zustand";
+
 export interface AuthState {
   token: string | null;
   setToken: (token: string) => void;
+  user: User | null;
+  setUser: (user: User) => void;
 }
 
-export const authReducer = (set): AuthState => ({
+export const authReducer: StateCreator<AuthState> = (set): AuthState => ({
   token: null,
   setToken: (token: string) => set({ token: token }),
+  user: null,
+  setUser: (user: User) => set({ user: user }),
 });
